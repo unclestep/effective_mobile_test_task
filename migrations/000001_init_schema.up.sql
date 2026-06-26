c1db33b21b578cac2155b432391e678e372048ed
Stepan <86169299+unclestep@users.noreply.github.com>
2026-06-27 00:26:35 +0300

    fix: remove unused postgres index

--- migrations/000001_init_schema.up.sql
+++ migrations/000001_init_schema.up.sql
@@ -9,4 +9,3 @@ CREATE TABLE subscriptions(
 );
 
 CREATE INDEX idx_subscriptions_user_id ON subscriptions (user_id);
-CREATE INDEX idx_subscriptions_user_service ON subscriptions (user_id, service_name);
